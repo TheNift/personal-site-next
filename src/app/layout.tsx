db4decc6c0da26cf05083e7c6bfbd42542545195
Jack Kill <jackkillschool@gmail.com>
2026-02-25 16:11:33 -0600

    - Fixed some deployment minuscea

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,25 @@ import LoadingHandler from '@/components/LoadingHandler';
 import '@/assets/index.css';
 
 export const metadata: Metadata = {
-	title: 'My Portfolio',
-	description: 'Personal portfolio website',
+	title: 'Jack Kill | Software Engineer & Constantly Learning',
+	description:
+		"I'm Jack Kill, a constantly-improving software engineer and computer enthusiast! This is my personal portfolio site, built to showcase my capabilities.",
 	icons: { icon: '/favicon.ico' },
+	openGraph: {
+		images: '/images/meta_image.webp',
+		siteName: 'Jack Kill | Software Engineer & Constantly Learning',
+		locale: 'en_US',
+		type: 'website',
+		url: 'https://jackkill.com',
+		description:
+			"I'm Jack Kill, a constantly-improving software engineer and computer enthusiast! This is my personal portfolio site, built to showcase my capabilities.",
+	},
+	alternates: {
+		languages: {
+			'en-US': 'https://jackkill.com',
+			'vi-VN': 'https://jackkill.com',
+		},
+	},
 };
 
 //  COOL IN THEORY, SLOWED DOWN LCP IN PRACTICE - re-added font-face declarations to index.css
@@ -80,23 +96,92 @@ export default function RootLayout({
 							pronouns: 'He/Him',
 							jobTitle: 'Software Engineer',
 							height: [
-								{ '@type': 'QuantitativeValue', value: '192', unitCode: 'cm', unitText: 'centimeters' },
-								{ '@type': 'QuantitativeValue', value: '75', unitCode: 'in', unitText: 'inches' },
-								{ '@type': 'QuantitativeValue', value: '6.25', unitCode: 'ft', unitText: 'feet' },
+								{
+									'@type': 'QuantitativeValue',
+									value: '192',
+									unitCode: 'cm',
+									unitText: 'centimeters',
+								},
+								{
+									'@type': 'QuantitativeValue',
+									value: '75',
+									unitCode: 'in',
+									unitText: 'inches',
+								},
+								{
+									'@type': 'QuantitativeValue',
+									value: '6.25',
+									unitCode: 'ft',
+									unitText: 'feet',
+								},
 							],
 							weight: [
-								{ '@type': 'QuantitativeValue', value: '97.5', unitCode: 'kg', unitText: 'kilograms' },
-								{ '@type': 'QuantitativeValue', value: '215', unitCode: 'lb', unitText: 'pounds' },
+								{
+									'@type': 'QuantitativeValue',
+									value: '97.5',
+									unitCode: 'kg',
+									unitText: 'kilograms',
+								},
+								{
+									'@type': 'QuantitativeValue',
+									value: '215',
+									unitCode: 'lb',
+									unitText: 'pounds',
+								},
 							],
 							hasOccupation: [
-								{ '@type': 'Role', hasOccupation: { '@type': 'Occupation', name: 'Software Engineer', occupationalCategory: '15-1252.00' }, startDate: '2024-07-01', endDate: 'present' },
-								{ '@type': 'Role', hasOccupation: { '@type': 'Occupation', name: 'Technology Intern', occupationalCategory: '15-1299.09' }, startDate: '2023-07-01', endDate: '2023-08-31' },
-								{ '@type': 'Role', hasOccupation: { '@type': 'Occupation', name: 'Software Engineer Intern', occupationalCategory: '15-1252.00' }, startDate: '2022-07-01', endDate: '2024-01-31' },
-								{ '@type': 'Role', hasOccupation: { '@type': 'Occupation', name: 'SELF Fellow', occupationalCategory: '25-1199.00' }, startDate: '2019-08-01', endDate: '2024-05-31' },
+								{
+									'@type': 'Role',
+									hasOccupation: {
+										'@type': 'Occupation',
+										name: 'Software Engineer',
+										occupationalCategory: '15-1252.00',
+									},
+									startDate: '2024-07-01',
+									endDate: 'present',
+								},
+								{
+									'@type': 'Role',
+									hasOccupation: {
+										'@type': 'Occupation',
+										name: 'Technology Intern',
+										occupationalCategory: '15-1299.09',
+									},
+									startDate: '2023-07-01',
+									endDate: '2023-08-31',
+								},
+								{
+									'@type': 'Role',
+									hasOccupation: {
+										'@type': 'Occupation',
+										name: 'Software Engineer Intern',
+										occupationalCategory: '15-1252.00',
+									},
+									startDate: '2022-07-01',
+									endDate: '2024-01-31',
+								},
+								{
+									'@type': 'Role',
+									hasOccupation: {
+										'@type': 'Occupation',
+										name: 'SELF Fellow',
+										occupationalCategory: '25-1199.00',
+									},
+									startDate: '2019-08-01',
+									endDate: '2024-05-31',
+								},
 							],
 							knowsLanguage: [
-								{ '@type': 'Language', name: 'English', alternateName: 'Tiếng Anh' },
-								{ '@type': 'Language', name: 'Vietnamese', alternateName: 'Tiếng Việt' },
+								{
+									'@type': 'Language',
+									name: 'English',
+									alternateName: 'Tiếng Anh',
+								},
+								{
+									'@type': 'Language',
+									name: 'Vietnamese',
+									alternateName: 'Tiếng Việt',
+								},
 							],
 							skills: [
 								{ '@type': 'Skill', name: 'JavaScript' },
@@ -108,42 +193,160 @@ export default function RootLayout({
 								{ '@type': 'Skill', name: 'Python' },
 								{ '@type': 'Skill', name: 'Java' },
 								{ '@type': 'Skill', name: 'C#' },
-								{ '@type': 'Skill', name: 'React', alternateName: 'Vite, Next.js, NextJS' },
-								{ '@type': 'Skill', name: 'React Native', alternateName: 'React-Native, Expo' },
-								{ '@type': 'Skill', name: 'Node.js', alternateName: 'NodeJS' },
-								{ '@type': 'Skill', name: 'Tailwind CSS', alternateName: 'TailwindCSS' },
+								{
+									'@type': 'Skill',
+									name: 'React',
+									alternateName: 'Vite, Next.js, NextJS',
+								},
+								{
+									'@type': 'Skill',
+									name: 'React Native',
+									alternateName: 'React-Native, Expo',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Node.js',
+									alternateName: 'NodeJS',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Tailwind CSS',
+									alternateName: 'TailwindCSS',
+								},
 								{ '@type': 'Skill', name: 'MongoDB' },
-								{ '@type': 'Skill', name: 'MySQL', alternateName: 'SQL' },
-								{ '@type': 'Skill', name: 'Full-Stack Development', alternateName: 'Fullstack Development' },
-								{ '@type': 'Skill', name: 'Frontend Development' },
-								{ '@type': 'Skill', name: 'Backend Development' },
-								{ '@type': 'Skill', name: 'Unity', alternateName: 'Unity Engine' },
+								{
+									'@type': 'Skill',
+									name: 'MySQL',
+									alternateName: 'SQL',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Full-Stack Development',
+									alternateName: 'Fullstack Development',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Frontend Development',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Backend Development',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Unity',
+									alternateName: 'Unity Engine',
+								},
 								{ '@type': 'Skill', name: 'Unreal Engine' },
-								{ '@type': 'Skill', name: 'Godot', alternateName: 'Godot Engine' },
-								{ '@type': 'Skill', name: 'AWS', alternateName: 'Amazon Web Services' },
+								{
+									'@type': 'Skill',
+									name: 'Godot',
+									alternateName: 'Godot Engine',
+								},
+								{
+									'@type': 'Skill',
+									name: 'AWS',
+									alternateName: 'Amazon Web Services',
+								},
 								{ '@type': 'Skill', name: 'Docker' },
 								{ '@type': 'Skill', name: 'Git' },
-								{ '@type': 'Skill', name: 'GitHub', alternateName: 'GitHub Actions' },
-								{ '@type': 'Skill', name: 'Cloudflare', alternateName: 'Cloudflare Workers' },
+								{
+									'@type': 'Skill',
+									name: 'GitHub',
+									alternateName: 'GitHub Actions',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Cloudflare',
+									alternateName: 'Cloudflare Workers',
+								},
 								{ '@type': 'Skill', name: 'Cloudflare Pages' },
 								{ '@type': 'Skill', name: 'WordPress' },
-								{ '@type': 'Skill', name: 'Shopify', alternateName: 'Shopify API' },
-								{ '@type': 'Skill', name: 'Liquid', alternateName: 'Liquid Template Language' },
-								{ '@type': 'Skill', name: 'Selenium', alternateName: 'Selenium WebDriver' },
-								{ '@type': 'Skill', name: 'REST API', alternateName: 'RESTful API' },
+								{
+									'@type': 'Skill',
+									name: 'Shopify',
+									alternateName: 'Shopify API',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Liquid',
+									alternateName: 'Liquid Template Language',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Selenium',
+									alternateName: 'Selenium WebDriver',
+								},
+								{
+									'@type': 'Skill',
+									name: 'REST API',
+									alternateName: 'RESTful API',
+								},
 								{ '@type': 'Skill', name: 'UI/UX Development' },
-								{ '@type': 'Skill', name: 'Linux', alternateName: 'Arch Linux, Ubuntu, Manjaro' },
-								{ '@type': 'Skill', name: 'MacOS', alternateName: 'macOS' },
-								{ '@type': 'Skill', name: 'Windows', alternateName: 'Windows 10, Windows 11' },
-								{ '@type': 'Skill', name: 'Android', alternateName: 'Android OS' },
-								{ '@type': 'Skill', name: 'iOS', alternateName: 'iOS OS' },
-								{ '@type': 'Skill', name: 'xCode', alternateName: 'xCode IDE' },
-								{ '@type': 'Skill', name: 'VSCode', alternateName: 'Visual Studio Code' },
-								{ '@type': 'Skill', name: 'Teamwork', alternateName: 'Communication, Leadership, Collaboration' },
-								{ '@type': 'Skill', name: 'Problem Solving', alternateName: 'Problem Solving, Critical Thinking' },
-								{ '@type': 'Skill', name: 'Adaptability', alternateName: 'Adaptability, Flexibility' },
-								{ '@type': 'Skill', name: 'Time Management', alternateName: 'Time Management, Prioritization' },
-								{ '@type': 'Skill', name: 'Agile Development', alternateName: 'Agile Methodologies, Scrum' },
+								{
+									'@type': 'Skill',
+									name: 'Linux',
+									alternateName:
+										'Arch Linux, Ubuntu, Manjaro',
+								},
+								{
+									'@type': 'Skill',
+									name: 'MacOS',
+									alternateName: 'macOS',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Windows',
+									alternateName: 'Windows 10, Windows 11',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Android',
+									alternateName: 'Android OS',
+								},
+								{
+									'@type': 'Skill',
+									name: 'iOS',
+									alternateName: 'iOS OS',
+								},
+								{
+									'@type': 'Skill',
+									name: 'xCode',
+									alternateName: 'xCode IDE',
+								},
+								{
+									'@type': 'Skill',
+									name: 'VSCode',
+									alternateName: 'Visual Studio Code',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Teamwork',
+									alternateName:
+										'Communication, Leadership, Collaboration',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Problem Solving',
+									alternateName:
+										'Problem Solving, Critical Thinking',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Adaptability',
+									alternateName: 'Adaptability, Flexibility',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Time Management',
+									alternateName:
+										'Time Management, Prioritization',
+								},
+								{
+									'@type': 'Skill',
+									name: 'Agile Development',
+									alternateName: 'Agile Methodologies, Scrum',
+								},
 							],
 							alumniOf: {
 								'@type': 'EducationalOrganization',
